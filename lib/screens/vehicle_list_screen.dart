3162483f1374b9c9b058ba59f0076b08a0b88bf7
Melanie <mel.lontsi@gmail.com>
2021-10-23 14:30:33 -0700

    added bottom navbar to VehiclePage

--- lib/screens/vehicle_list_screen.dart
+++ lib/screens/vehicle_list_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:project_auto_design/widgets/custom_bottom_navbar.dart';
 
 import '../values/colors.dart';
+import 'home_screen.dart';
 
 class VehicleListScreen extends StatefulWidget {
   String searchQuery;
@@ -15,15 +17,39 @@ class VehicleListScreen extends StatefulWidget {
 }
 
 class _VehicleListScreenState extends State<VehicleListScreen> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget _getPage(int index) {
+    if (index != 0) return _emptyScreen();
+    return Home();
+  }
+
+  Widget _emptyScreen() {
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: APP_BG,
+            elevation: 0.0,
+            centerTitle: true,
+            title: Text("Intentionally left Blank")));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            elevation: 0.0,
-            centerTitle: true,
-            backgroundColor: Colors.white70,
-            title: Text("Vehicle Type", style: TextStyle(color: APP_BG))),
-        body: Container(color: APP_BG, child: _buildGrid()));
+      appBar: AppBar(
+          elevation: 0.0,
+          centerTitle: true,
+          backgroundColor: Colors.white70,
+          title: Text("Vehicle Type", style: TextStyle(color: APP_BG))),
+      body: Container(color: APP_BG, child: _buildGrid()),
+      bottomNavigationBar: CustomBottomNavBar(_selectedIndex, _onItemTapped),
+    );
   }
 
   Widget _buildGrid() {
