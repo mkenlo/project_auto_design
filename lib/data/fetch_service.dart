f5da5f96c8ba93ea5f2d97f6235248c2986c81df
Melanie <mel.lontsi@gmail.com>
2021-10-25 11:21:24 -0700

    implemented special search function

--- lib/data/fetch_service.dart
+++ lib/data/fetch_service.dart
@@ -4,10 +4,9 @@ import 'package:flutter/services.dart';
 
 import 'models/vehicle_model.dart';
 
-// @TODO implement filter features
+/*Parameter <query> is a string of parameters (Make Model Year) space separated. Example: Nissan Rogue 2013*/
 Future fetchAndFilterVehicles(String query) async {
-  final jsonObj = await rootBundle.loadString("assets/data/data.json");
-  List<Vehicle> vehicles = Vehicle.asListFromJson(json.decode(jsonObj));
+  List<Vehicle> vehicles = await fetchVehicles();
   final params = cleanQueryString(query);
   if (params.length >= 1) vehicles = filterByMake(vehicles, params[0]);
   if (params.length >= 2) vehicles = filterByModel(vehicles, params[1]);
@@ -33,5 +32,46 @@ List<Vehicle> filterByModel(List vehicles, String model) {
 }
 
 List<Vehicle> filterByYear(List vehicles, String year) {
-  return vehicles.where((item) => item.year.startsWith(year)).toList();
+  return vehicles.where((item) => item.year == int.parse(year)).toList();
+}
+
+List<Vehicle> filterByPrice(List vehicles, String price) {
+  return vehicles.where((item) => item.price <= int.parse(price)).toList();
+}
+
+Future fetchVehicles() async {
+  final jsonObj = await rootBundle.loadString("assets/data/data.json");
+  List<Vehicle> vehicles = Vehicle.asListFromJson(json.decode(jsonObj));
+  return vehicles;
+}
+
+Future<Map<String, num>> searchVehicles(
+    {String make, String model, String year, String price}) async {
+  Map res = {
+    'total_vehicles': 0,
+    'lower_price': 0,
+    'highest_price': 0,
+    'median_price': 0
+  };
+  List<Vehicle> vehicles = await fetchVehicles();
+  if (make != null && make.isNotEmpty) vehicles = filterByMake(vehicles, make);
+  if (model != null && model.isNotEmpty)
+    vehicles = filterByModel(vehicles, model);
+  if (year != null && year.isNotEmpty) vehicles = filterByYear(vehicles, year);
+  if (price != null && price.isNotEmpty)
+    vehicles = filterByPrice(vehicles, price);
+
+  if (vehicles != null && vehicles.isNotEmpty) {
+    vehicles.sort((car1, car2) => car1.price.compareTo(car2.price));
+    res['total_vehicles'] = vehicles.length;
+    res['lower_price'] = vehicles.first;
+    res['highest_price'] = vehicles.last;
+    int mid = vehicles.length ~/ 2;
+    res['median_price'] = vehicles.elementAt(mid).price;
+    if (vehicles.length % 2 == 0) {
+      res['median_price'] =
+          (res['median_price'] + vehicles.elementAt(mid - 1).price) / 2;
+    }
+  }
+  return res;
 }
